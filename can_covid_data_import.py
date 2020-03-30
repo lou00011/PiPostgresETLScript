a4b4a4ca562563a3f1749c2d61ee102604f8c2e5
Phil <plrtn@protonmail.com>
2020-03-30 12:38:32 -0400

    Formatted

--- can_covid_data_import.py
+++ can_covid_data_import.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from secret import conn_str
 
-# from secret import conn_str
-# engine = create_engine(conn_str)
-
+engine = create_engine(conn_str)
 schema_name = "external"
 table_name = "covid_canada"
 url = "https://docs.google.com/spreadsheets/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv"
@@ -24,10 +23,11 @@ def get_full_table_name(schema_name, table_name):
 def test_print_dataframe(pandas_data_frame):
     print(pandas_data_frame.to_string(max_rows=5))
 
+
 def run():
     pdf = read_csv_from_url(url)
     full_table_name = get_full_table_name(schema_name, table_name)
-    test_print_dataframe(pdf)
+    to_postgres(pdf, full_table_name, engine)
 
 
 run()
